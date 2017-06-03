40032d5ffa7d4f1c8fd079f33d94288456f5cac0
john sander <jrjackets112@gmail.com>
2017-06-02 23:08:46 -0400

    updates

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+var orm = require('../config/orm.js');
+
+var burger_call = {
+  insertOne: function(some_val, cb){
+    orm.insert('burgers', 'burger_name', some_val, function(res){
+      cb(res);
+    });
+  },
+  selectAll: function(cb){
+    orm.read('burgers', function(res){
+      cb(res);
+    });
+  },
+  updateOne: function(user_id, cb){
+    orm.update('burgers', 'devoured', 1 , 'id', user_id, function(res){
+      cb(res);
+    });
+  }
+}
+
+module.exports = burger_call;
